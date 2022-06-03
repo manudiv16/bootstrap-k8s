c227eea9120382394cfe271aaa47f96882801a4c
Fran <38869988+manudiv16@users.noreply.github.com>
2022-06-03 09:11:11 +0200

    remove resources

--- 02-kubeconfig.tf
+++ 02-kubeconfig.tf
@@ -3,12 +3,3 @@ resource "local_file" "kubernetes_config" {
   filename = "kubeconfig.yaml"
 }
 
-
-resource "null_resource" "kubeconfig" {
-  depends_on = [scaleway_k8s_pool.prod-pool] # at least one pool here
-  triggers = {
-    host                   = scaleway_k8s_cluster.prod-cluster.kubeconfig[0].host
-    token                  = scaleway_k8s_cluster.prod-cluster.kubeconfig[0].token
-    cluster_ca_certificate = scaleway_k8s_cluster.prod-cluster.kubeconfig[0].cluster_ca_certificate
-  }
-}
