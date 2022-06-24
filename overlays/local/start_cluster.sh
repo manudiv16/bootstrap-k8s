c96ada3b618e9bc85e94ca07a5ee378d30d26fbe
Fran <38869988+manudiv16@users.noreply.github.com>
2022-06-25 01:31:38 +0200

    changes in local overlay

--- overlays/local/start_cluster.sh
+++ overlays/local/start_cluster.sh
@@ -1,6 +1,8 @@
-kind create cluster --config ./overlays/local/kind.yml
-sleep 10
-kubectl apply -f ../../secret-ssh.yml
-kubectl create namespace argocd
+# kind create cluster --config ./overlays/local/kind.yml
+
+# sleep 20
+kubectl create namespace argocd 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 20
 kubectl apply -k ./bootstrap/overlays/local
+kubectl apply -f ../../secret-ssh.yml
