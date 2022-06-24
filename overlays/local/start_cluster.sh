96365e02db01c688a7fcff16d427040f3a95c86d
Fran <38869988+manudiv16@users.noreply.github.com>
2022-06-24 20:51:50 +0200

    change branch prometheus app

--- overlays/local/start_cluster.sh
+++ overlays/local/start_cluster.sh
@@ -1,4 +1,5 @@
 kind create cluster --config ./overlays/local/kind.yml
+kubectl apply -f ../../secret-ssh.yml
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -k ./bootstrap/overlays/local
